56ea3064be4ac4a7bf8c2c65d3f256f2cf536d92
Paulius Jonikavicius <pjon@itu.dk>
2014-10-17 20:54:42 +0200

    Restructure

--- TwinKing.lua
+++ TwinKing.lua
@@ -1,3 +1,41 @@
-function TwinKing.MSG.receive(from, data)
---    validate 'from'
-end
+local toc, data = ...
+local addonID = toc.identifier
+local TwinKing
+
+TwinKing = {
+    event = {
+        -- Message received
+        messageReceived = function(handle, from, type, channel, identifier, data)
+            if identifier == addonID then
+                -- TODO: call function based on data
+                TwinKing.MSG:received(from, data)
+            end
+        end
+    },
+    MSG = {
+        --[[
+        -- Checks if the message was broadcaster by correct person
+        -- ]]
+        isValidBroadcaster = function(this, from)
+        -- User is the broadcaster
+            if from == Inspect.Unit.Detail("player").name then
+                return false
+            end
+
+        -- Broadcaster is not a member of the guild
+
+        end,
+        --[[
+        -- Broadcasted message receiver
+        -- ]]
+        received = function(this, from, data)
+        --    validate 'from'
+            if this:isValidBroadcaster(from) then
+                -- TODO: update information
+            end
+        end
+    }
+}
+
+Command.Event.Attach(Event.Message.Receive, TwinKing.event.messageReceived, "Message received")
+print(Inspect.Guild.Roster.Detail("Uogutes"))
