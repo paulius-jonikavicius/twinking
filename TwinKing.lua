4a67788eb69e767b52ac7c76f57b1dc154f5f1fe
Paulius Jonikavicius <pjon@itu.dk>
2014-10-18 15:53:33 +0200

    Broadcast

--- TwinKing.lua
+++ TwinKing.lua
@@ -1,14 +1,14 @@
 local toc, data = ...
 local addonID = toc.identifier
 local TwinKing
+local playerID = Inspect.Unit.Lookup("player") -- get player id
 
 local dummyDB = {
     warrior = {
-        [1] = {
-            name = "Uogute",
-            lastReceived = nil
-        },
-        [2] = { name = "Paseles" }
+        [1] = "Uogute",
+        [2] = "Paseles",
+        ["Uogute"] = nil,
+        ["Paseles"] = nil
     },
     cleric = {},
     rogue = {},
@@ -22,7 +22,9 @@ TwinKing = {
         ["Crusader"] = true
     },
     event = {
-        -- Message received
+        --[[
+        --  Handles broadcasted messages
+        -- ]]
         messageReceived = function(handle, from, type, channel, identifier, data)
             if identifier == addonID then
                 -- TODO: call function based on data
@@ -33,26 +35,34 @@ TwinKing = {
     DB = {
         --[[
         --  Checks if player is in the database.
-        --  Returns position and player or nil
+        --  Returns position and player name or nil
         -- ]]
         contains = function(player)
-            for i, v in ipairs(tKingDB[player.class]) do
-                if v.name == player.name then
-                    return v, i
+            if tKingDB[player.class][player.name] then
+                for i, v in ipairs(tKingDB[player.class]) do
+                    if v == player.name then
+                        return v, i
+                    end
                 end
             end
+
             return nil
         end,
         --[[
         --  Kills the 'king' by moving him to the end
-        --  of the list
+        --  of the list and updating time
+        --  @data = {}
+        --      class   = string
+        --      name    = string
+        --  @pos = int
         -- ]]
-        kill = function(king, pos)
-            while pos < #tKingDB do
-                tKingDB[pos] = tKingDB[pos + 1]
+        kill = function(data, pos)
+            while pos < #tKingDB[data.class] do
+                tKingDB[data.class][pos] = tKingDB[data.class][pos + 1]
                 pos = pos + 1
             end
-            tKingDB[pos] = king
+            tKingDB[data.class][pos] = data.name
+            tKingDB[data.class][data.name] = Inspect.Time.Server() -- update time
         end
     },
     MSG = {
@@ -81,22 +91,34 @@ TwinKing = {
         --[[
         -- Broadcasted message receiver
         -- @from = string, player name
-        -- @data = {
+        -- @data = {}
         --  class   = string
         --  name    = string
         -- ]]
         received = function(this, from, data)
         --    validate 'from'
             if this:isValidBroadcaster(from) then
-                local king, pos = TwinKing.DB.contains(decode(data))
+                local data = decode(data)
+                local king, pos = TwinKing.DB.contains(data)
 
                 if king ~= nil then
-                    king.lastReceived = Inspect.Time.Server()
-                    TwinKing.DB.kill(king, pos)
+                    TwinKing.DB.kill(data, pos)
                 end
             end
-        end
-    }
+        end,
+        broadcast = {
+            kill = function(name)
+                detail = Inspect.Guild.Roster.Detail(name)
+                king = Inspect.Unit.Detail(detail.id)
+            end
+        }
+    },
+    test = function(h, t, c, a)
+
+    end
 }
 
 Command.Event.Attach(Event.Message.Receive, TwinKing.event.messageReceived, "Message received")
+Command.Event.Attach(Event.Unit.Availability.Full, TwinKing.test, "Test King")
+
+--print_r(Inspect.Unit.Lookup("Uogute"))
